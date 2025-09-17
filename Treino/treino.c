5efada7c5036ebba3fcc0a3f074803f8d8ebf344
Robson <rbispo.eng@gmail.com>
2025-09-17 00:01:00 +0000

    testes em treino

--- Treino/treino.c
+++ Treino/treino.c
@@ -12,4 +12,33 @@ int main()
             printf("matriz[%d][%d] %d\n", i, j, matriz[i][j]);
         }
     }
+
+    int num1, num2;
+    int soma;
+    int multiplicacao;
+
+    printf("Digite o 1o número: ");
+    scanf("%d", &num1);
+    printf("Digite o segundo número: ");
+    scanf("%d", &num2);
+
+    if (num1 > num2)
+    {
+        soma = num1 + num2;
+        printf("A soma dos números é %d\n", soma);
+    }
+    else if (num1 < num2)
+    {
+        multiplicacao = num2 * num1;
+        printf("O produto dos números é: %d\n", multiplicacao);
+    }
+    else
+    {
+        printf("Números iguais.\n");
+    }
+
+
+
+
+    return 0;
 }
