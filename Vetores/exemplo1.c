11e80235a94bca3b8c62db2b6a48ada517b2c925
Robson <rbispo.eng@gmail.com>
2025-09-23 03:02:00 +0000

    Adicionado comentários no exemplo1 de vetores

--- Vetores/exemplo1.c
+++ Vetores/exemplo1.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// Este programa soma os elementos do vetor e exibe o resultado
+
 int main()
 {
     int vetor[5] = {1,2,3,4,5};
@@ -7,7 +9,7 @@ int main()
 
     for (int i = 0; i < 5; i++)
     {
-        soma += vetor[i];
+        soma += vetor[i]; // Somador (soma = soma + vetor[i])
     }
 
     printf("Soma dos valores do vetor: %d\n", soma);
