67944909e083c8bdc4785267152cd0c167aca1ce
Robson <rbispo.eng@gmail.com>
2026-02-18 00:20:28 +0000

    Criando o exemplo e adicionando comentários

--- Structs/Exemplo1.c
+++ Structs/Exemplo1.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
-int main(){
-
-struct Aluno {
+// Definindo uma estrutura para um Aluno
+typedef struct {
     char nome[50];
     int idade;
     float media;
+} Aluno;
 
-};
+
+int main(){
+
+// Criando e inicializando uma variável do tipo Aluno
+Aluno aluno1 = {"João", 20, 9.5};
+
+// Acessando os dados com o operador '.' (ponto)
+printf("Aluno: %s\n", aluno1.nome);
+printf("Idade: %d\n", aluno1.idade);
+printf("Nota: %.1f\n", aluno1.media);
 
 
 return 0;
