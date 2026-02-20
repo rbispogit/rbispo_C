5f1caeb5a6bcdfcf15a042cf7eee97c89e322be0
Robson <rbispo.eng@gmail.com>
2026-02-20 00:18:44 +0000

    Inicio

---
+++ Ponteiros/Exemplo1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 10; // supondo que x está armazenado no endereço de memória 01F
+    int* p = &x; // a variavel ponteiro p, recebe o endereço de x (01F)
+
+    printf("Valor de x: %d\n", x);
+    printf("Endereço de x: %p\n", &x);
+    printf("Conteúdo de p (o endereço): %p\n", p);
+    printf("Valor apontado por p: %d\n", *p);
+
+    *p = 20; // mando alterar o valor de x nesse endereço de memória
+    
+    printf("Novo valor de x: %d\n", x);
+
+    return 0;
+}
