7610f8808972b982124be5c3cb89112a015806c0
Robson <rbispo.eng@gmail.com>
2026-02-22 21:12:45 +0000

    Criei o Livre.c

---
+++ Livre/Livre.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+struct Profile
+{
+    char nome[20];
+    int força;
+    int inteligencia;
+    int agilidade;
+};
+
+int main()
+{
+    struct Profile personagem1 = {"Ragvar", 25, 5, 10};
+    struct Profile personagem2 = {"Merlins", 3, 25, 8};
+    struct Profile personagem3 = {"Felris", 14, 10, 25};
+    int opcao = 0;
+
+    printf("********************\n");
+    printf("***  THE PLAGUE  ***\n");
+    printf("********************\n\n");
+    printf("   - Start Game -     ");
+
+    /*switch (opcao)
+    {
+
+    }*/
+
+    return 0;
+}
