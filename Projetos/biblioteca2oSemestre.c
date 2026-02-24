eff24496fabc5ce6c810ca107b39d537eca7aef0
Robson <rbispo.eng@gmail.com>
2026-02-24 02:55:45 +0000

    Inicio do projeto biblioteca

---
+++ Projetos/biblioteca2oSemestre.c
@@ -0,0 +1,21 @@
+// ===============================================================================
+// PRÁTICA: SISTEMA DE BIBLIOTECA
+
+// OBJETIVO:
+// Adicionar a funcionalidade de empréstimo e introduzir alocação dinâmica.
+// - Os arrays de livros e empréstimos são alocados com malloc/calloc.
+// - Nova struct para Empréstimo.
+// - Liberação de memória com free.
+// ===============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// --- Constantes Globais ---
+#define MAX_LIVROS 50;
+#define MAX_EMPRESTIMOS 100; // define a capacidade para empréstimos.
+#define TAM_STRING 100;
+
+// --- Definição das Estruturas ---
+// Struct Livro tem um campo 'disponível' para controlar o status.
