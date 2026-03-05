b4bf6971b1328ad9fcc2a2b5fd424732f1a646c0
Robson <rbispo.eng@gmail.com>
2026-03-05 00:48:21 +0000

    Criei o exemplo1 de listas

---
+++ Listas/Exemplo1.c
@@ -0,0 +1,25 @@
+// Listas Lineares: estáticas e dinâmicas
+
+#include <stdio.h>
+
+int main()
+
+{
+    // Arranja um espaço na memória para guardar 3 números inteiros
+    int* numeros = (int*) malloc(3 * sizeof(int));
+
+    // Criando uma lista
+    numeros[0] = 10;
+    numeros[1] = 20;
+    numeros[2] = 30;
+
+    // Expandindo a lista
+    numeros = (int*) realloc(numeros, 5 * sizeof(int));
+
+    // Adicionando itens a lista
+    numeros[3] = 40;
+    numeros[4] = 50;
+
+
+    return 0;
+}
