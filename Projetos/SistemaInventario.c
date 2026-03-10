aef8c87cca847921a75311c0b107d5a09608ce6c
Robson <rbispo.eng@gmail.com>
2026-03-10 22:28:44 +0000

    Continuando o projeto SistemaInventario

--- Projetos/SistemaInventario.c
+++ Projetos/SistemaInventario.c
@@ -19,5 +19,35 @@ typedef struct
     int quantidade; // Este é o nosso contador de controle. Ele informa quantos itens estão realmente armazenados na lista 
 } ListaEstatica;
 
+// Funções da Lista Estática
+
+// Esta função prepara a lista pra ser usada.
+// Ela simplesmente define o contador quantidade como 0, indicando que a lista está vazia.
+// Recebemos um ponteiro (*lista) para modificar a variável original passada para a função.
+void inicializarListaEstatica(ListaEstatica *lista);
+
+void inserirListaEstatica(ListaEstatica *lista, const char* texto);
+void removerListaEstatica(ListaEstatica *lista, const char* texto);
+void listarListaEstatica(const ListaEstatica *lista);
+
+// ----------------------------------------
+// PARTE 2: LISTA ENCADEADA (DE STRINGS)
+// ----------------------------------------
+
+// Estrutura do nó (cada elemento da lista)
+// - char* dado: Este é um ponteiro. Ele não armazena o texto em si, mas sim o endereço
+// de memória onde o texto está guardado.
+// Isso é muito flexível, pois cada texto pode ter um tamanho diferente, e alocamos apenas
+// a memória necessária para cada um.
+// - struct No *proximo: Este é o ponteiro que cria a "ligação". Ele armazena o endereço do próximo nó na lista.
+// O último nó da lista terá este ponteiro apontando para NULL.
+typedef struct No
+{
+  char* dado;
+  struct No *proximo; // Ponteiro para a string (alocada dinamicamente)
+} No;
+
+
+
 
 
